e2d5dc6aa19b10761b26c9930b1526ea652950d2
Doron Raifman <draifman@gmail.com>
2020-05-04 19:36:19 +0300

    multi processing example

--- Learn/MultiProcessing/ProcessPool.py
+++ Learn/MultiProcessing/ProcessPool.py
@@ -47,11 +47,39 @@ def example4():
     pool.join()
 
 
+def example5():
+    print("example 5")
+    pool = Pool(processes=4)
+    results = pool.map(f, [5, 10])
+    pool.close()
+    pool.join()
+    print(results)
+
+
+def example6():
+    print("example 6")
+    pool = Pool(processes=4)
+
+    # add many job quantum tasks to be performed
+    result_handles = []
+    for start_value in range(10, 40+1, 10):
+        values_list = list(range(start_value, start_value+10))
+        for value in values_list:
+            res_handle = pool.apply_async(f, [value])
+            result_handles.append(res_handle)
+
+    # wait for completed tasks to finish (one by one)
+    # note that the execution order is not the order you put the tasks
+    for result_handle in result_handles:
+        results = result_handle.get(timeout=20)
+        print(results)
+
+    pool.close()
+    pool.join()
+
+
 if __name__ == '__main__':
-    # example1()
-    # example2()
-    # example3()
-    example4()
+    example6()
 
 
 
