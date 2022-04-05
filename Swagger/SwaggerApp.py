388a454fdabf17ebbe9f0005a5b29bc5d85a1f4e
Doron Raifman <draifman@gmail.com>
2022-04-05 17:57:19 +0300

    Swagger

--- Swagger/SwaggerApp.py
+++ Swagger/SwaggerApp.py
@@ -22,8 +22,9 @@ def index():
 # http://127.0.0.1:5000/book_details/1234567890/HarryPotter/2009
 @app.route('/book_details/<string:book_id>/<string:book_name>/<int:book_year>', methods=['GET'])
 def book_details(book_id: str, book_name: str, book_year: int) -> str:
+    print(f'/book_details/{book_id}/{book_name},{book_year}')
     result = SwaggerApp.worker.book_details(book_id, book_name, book_year)
-    print(f'/book_details, result: {result}')
+    print(f'\tresult: {result}')
     return jsonify(result)
 
 
