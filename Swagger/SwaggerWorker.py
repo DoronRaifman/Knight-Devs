7959c5a5032d8209515704f437e888e8113f4c14
Doron Raifman <draifman@gmail.com>
2022-04-06 13:03:57 +0300

    add some Todo

--- Swagger/SwaggerWorker.py
+++ Swagger/SwaggerWorker.py
@@ -26,36 +26,38 @@ class SwaggerWorker:
 
     def test_swagger(self):
         file_name = os.path.join('Data', 'Swagger.yaml')
+        yaml_data = None
         with open(file_name, "rt") as stream:
             try:
                 yaml_data = yaml.safe_load(stream)
-                host, schema = yaml_data['host'], yaml_data['schemes'][0]
-                paths = yaml_data['paths']
-                path_key = list(paths.keys())[0]
-                path_all_data = paths[path_key]
-                path_data = path_all_data['get']
-                produces = path_data['produces'][0]
-                parameters = path_data['parameters']
-                params_url = str(path_key)
-                params_url = params_url.replace('{', '')
-                params_url = params_url.replace('}', '')
-                for parameter in parameters:
-                    param_name = parameter['name']
-                    value = self.book_result[param_name]
-                    true_value = str(value)
-                    params_url = params_url.replace(param_name, true_value, 1)
-                url = f'{schema}://{host}:5000{params_url}'
-                print(f'url:{url}')
-                header = {'accept': produces}
-                result = requests.get(url, headers=header)
-                json_dict_res = json.loads(result.text)
-                if json_dict_res != self.book_result:
-                    print(f'response error. got {result}')
-                else:
-                    print('worked ok')
-
             except yaml.YAMLError as ex:
                 print(ex)
+        if yaml_data is not None:
+            paths = yaml_data['paths']
+            path_key = list(paths.keys())[0]
+            # Todo: handle also other paths
+            path_all_data = paths[path_key]
+            path_data = path_all_data['get']
+            # Todo: handle also other requests (not only get)
+            produces = path_data['produces'][0]
+            # Todo: handle also xml
+            parameters = path_data['parameters']
+            params_url = str(path_key)      # clone
+            for parameter in parameters:
+                param_name = parameter['name']
+                value = str(self.book_result[param_name])
+                params_url = params_url.replace('{' + param_name + '}', value, 1)
+            host, schema = yaml_data['host'], yaml_data['schemes'][0]
+            # Todo: handle also https
+            url = f'{schema}://{host}:5000{params_url}'
+            print(f'url:{url}')
+            header = {'accept': produces}
+            result = requests.get(url, headers=header)
+            json_dict_res = json.loads(result.text)
+            if json_dict_res != self.book_result:
+                print(f'response error. got {result}')
+            else:
+                print('worked ok')
 
 
 
